2c31b7c834daf06a2f5f6c2c8a6d4c8dd82a852d
jorgebackend73-lang <jorgebackend73@gmail.com>
2026-04-24 11:50:15 +0200

    Antes de iterar/recorrer las colecciones

--- collection_framework/src/main/java/com/example/App.java
+++ collection_framework/src/main/java/com/example/App.java
@@ -34,61 +34,55 @@ public class App {
     /*Ejemplo de creación de lista (colección) utilizando el metodo add(), que es 
      * polimorfico, para agregar elementos a la lista */
     
-    // List<Persona> personas = new ArrayList<>();
-    var personas = new ArrayList<Persona>(); // alternativa a la forma anterior var es un generico para List<Persona>
+    var personas = new ArrayList<Persona>();
+    // var personas = new ArrayList<Persona>(); // alternativa a la forma anterior var es un generico para List<Persona>
     // siempre que uses var hay que darle al compilador alguna pista de lo que realmente es.
     
-    
-    var persona1 = Persona.builder()
+    personas.add(
+    Persona.builder()
     	.nombre("Douglas")
     	.primerApellido("Tay")
     	.segundoApellido("Gon")
     	.fechaNacimiento(LocalDate.of(1995, Month.JANUARY, 20))
     	.genero(Genero.HOMBRE)
-    	.build();
+    	.build()
+    	);
     
-    var persona2 = Persona.builder()
+    personas.add(
+    Persona.builder()
     	.nombre("Carolina")
     	.primerApellido("Garzon")
     	.segundoApellido("Becerra")
     	.fechaNacimiento(LocalDate.of(2000, Month.OCTOBER, 10))
     	.genero(Genero.MUJER)
-    	.build();
+    	.build()
+    	);
     
-    var persona3 = Persona.builder()
+    personas.add(
+    Persona.builder()
         	.nombre("Maria")
         	.primerApellido("Garzon")
         	.segundoApellido("González")
         	.fechaNacimiento(LocalDate.of(2005, Month.OCTOBER, 22))
         	.genero(Genero.MUJER)
-        	.build();
+        	.build()
+        	);
     
-    var persona4 = Persona.builder()
+    personas.add(
+    Persona.builder()
         	.nombre("Jerónimo")
         	.primerApellido("Arenal")
         	.segundoApellido("Gomez")
         	.fechaNacimiento(LocalDate.of(1989, Month.MAY, 30))
         	.genero(Genero.HOMBRE)
-        	.build();
+        	.build()
+        	);
     
-    // Estamos usando variables que vamos a usar una sola vez para crear el array, nos sobran.
+   /*La lista resultante: personas no es de tamaño fijo, pues no la hemos obtenido a paratir de un array.
+    * Se le pueden añadir o quitar elemtos cuando sea necesario.
+    * */
     
-    personas.add(persona1); // esto así nos da null pointer exception por intentar agrgar un elemento sin reservar memoria.
-    personas.add(persona2);
-    personas.add(persona3);
-    personas.add(persona4);
-    
-    /* En lugar de invocar el metodo 4 veces, como arriba, se hace todo de una vez... o no.*/
-    
-    personas = (ArrayList<Persona>) Arrays.asList(persona1, persona2, persona3, persona4);
-    
-    
-    /* Si la lista de personas apunta a null,  entonces no podemos agregar elementos Persona
-     * a dicha lista, porque no hemos reservado memoria para la lista de personas
-     * 
-     * Para reservar memoria para la lista de persona
-     * Utilizando el constructor de alguna de las clases que implementa la interfaz List 
-     * */
+    System.out.println(personas);
     
     }
 }
