dc5c98495e0e7767978b15d0958730e6d65378f0
jorgebackend73-lang <jorgebackend73@gmail.com>
2026-04-24 11:22:24 +0200

    Antes de eliminar las variables persona1 a persona4.

--- collection_framework/src/main/java/com/example/App.java
+++ collection_framework/src/main/java/com/example/App.java
@@ -2,8 +2,11 @@ package com.example;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+// import lombok.var;
 
 /**
  * El Framework de Colecciones está en: 
@@ -31,11 +34,12 @@ public class App {
     /*Ejemplo de creación de lista (colección) utilizando el metodo add(), que es 
      * polimorfico, para agregar elementos a la lista */
     
-    List<Persona> personas = null;
+    // List<Persona> personas = new ArrayList<>();
+    var personas = new ArrayList<Persona>(); // alternativa a la forma anterior var es un generico para List<Persona>
+    // siempre que uses var hay que darle al compilador alguna pista de lo que realmente es.
     
     
-    
-    Persona persona1 = Persona.builder()
+    var persona1 = Persona.builder()
     	.nombre("Douglas")
     	.primerApellido("Tay")
     	.segundoApellido("Gon")
@@ -43,9 +47,48 @@ public class App {
     	.genero(Genero.HOMBRE)
     	.build();
     
-    persona.add(persona1); // esto así nos da null pointer exception por intentar agrgar un elemento sin reservar memoria.
+    var persona2 = Persona.builder()
+    	.nombre("Carolina")
+    	.primerApellido("Garzon")
+    	.segundoApellido("Becerra")
+    	.fechaNacimiento(LocalDate.of(2000, Month.OCTOBER, 10))
+    	.genero(Genero.MUJER)
+    	.build();
     
-    /*Si la lista de personas apunta a null (me falta este comentario, añadirlo al revisar grabación)*/
+    var persona3 = Persona.builder()
+        	.nombre("Maria")
+        	.primerApellido("Garzon")
+        	.segundoApellido("González")
+        	.fechaNacimiento(LocalDate.of(2005, Month.OCTOBER, 22))
+        	.genero(Genero.MUJER)
+        	.build();
+    
+    var persona4 = Persona.builder()
+        	.nombre("Jerónimo")
+        	.primerApellido("Arenal")
+        	.segundoApellido("Gomez")
+        	.fechaNacimiento(LocalDate.of(1989, Month.MAY, 30))
+        	.genero(Genero.HOMBRE)
+        	.build();
+    
+    // Estamos usando variables que vamos a usar una sola vez para crear el array, nos sobran.
+    
+    personas.add(persona1); // esto así nos da null pointer exception por intentar agrgar un elemento sin reservar memoria.
+    personas.add(persona2);
+    personas.add(persona3);
+    personas.add(persona4);
+    
+    /* En lugar de invocar el metodo 4 veces, como arriba, se hace todo de una vez... o no.*/
+    
+    personas = (ArrayList<Persona>) Arrays.asList(persona1, persona2, persona3, persona4);
+    
+    
+    /* Si la lista de personas apunta a null,  entonces no podemos agregar elementos Persona
+     * a dicha lista, porque no hemos reservado memoria para la lista de personas
+     * 
+     * Para reservar memoria para la lista de persona
+     * Utilizando el constructor de alguna de las clases que implementa la interfaz List 
+     * */
     
     }
 }
